b67b277a0253b03695664b5913c4d2dffe2bdf1d
Niladri <rickyrickado14@gmail.com>
2023-08-17 23:52:39 +0530

    initial

---
+++ src/Application/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+function Application() {
+  return (
+    <div>
+        <h1>Application</h1>
+        <p>This is a example code</p>
+    </div>
+  )
+}
+
+export default Application;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Application from "./Application/index.jsx";
+
+const appContainer = document.getElementById("app");
+
+ReactDOM.render(<Application />, appContainer);
